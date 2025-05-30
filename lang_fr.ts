d81ec4c8d62010b88af044c11538a2cf4cbea4b2
Grubuntu <56045316+Grubuntu@users.noreply.github.com>
2025-05-30 18:24:05 +0200

    Add files via upload

---
+++ lang_fr.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>Console</name>
+    <message>
+        <location filename="CheckUpdate.py" line="297"/>
+        <source>❌ Error retrieving {} : {}
+</source>
+        <translation>❌ Erreur de récupération {} : {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="359"/>
+        <source>This macro requires FreeCAD graphique interface.</source>
+        <translation>Cette macro nécessite l&apos;interface graphique de FreeCAD.</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="CheckUpdate.py" line="75"/>
+        <source>FreeCAD customised or unknown version</source>
+        <translation>Version inconnue ou customisée de FreeCAD</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="96"/>
+        <source>Error retrieving weekly online version:{}
+</source>
+        <translation>Erreur de récupération de la version weekly en ligne : {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="108"/>
+        <source>Error retrieving stable online version:{}
+</source>
+        <translation>Erreur de récupération de la version stable en ligne : {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="183"/>
+        <source>✅ Weekly update available : {} &gt; {}</source>
+        <translation>✅ Version weekly à jour disponible : {} &gt; {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="190"/>
+        <source>❌ Error recovering weekly version.</source>
+        <translation>❌ Erreur de récupération de la version weekly.</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="254"/>
+        <source>Reload</source>
+        <translation>Recharger</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="256"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>Status</name>
+    <message>
+        <location filename="CheckUpdate.py" line="248"/>
+        <source>Loading...</source>
+        <translation>Chargement...</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="270"/>
+        <source>Updates checked.</source>
+        <translation>Mise à jour vérifiées.</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="274"/>
+        <source>Last check {} minutes ago.</source>
+        <translation>Dernière vérification il y a {} minutes.</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="281"/>
+        <source>Updated list.</source>
+        <translation>Liste à jour.</translation>
+    </message>
+</context>
+<context>
+    <name>Table</name>
+    <message>
+        <location filename="CheckUpdate.py" line="238"/>
+        <source>File name</source>
+        <translation>Nom de fichier</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="238"/>
+        <source>Action</source>
+        <translation>Action</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="299"/>
+        <source>Canal Stable :</source>
+        <translation>Canal Stable :</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="300"/>
+        <source>Canal Development :</source>
+        <translation>Canal Developpement :</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="321"/>
+        <source>Download</source>
+        <translation>Télécharger</translation>
+    </message>
+</context>
+<context>
+    <name>Tooltip</name>
+    <message>
+        <location filename="CheckUpdate.py" line="175"/>
+        <source>Unable to determine the version.</source>
+        <translation>Impossible de déterminer la version.</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="197"/>
+        <source>✅ Stable update available : {} &gt; {}</source>
+        <translation>✅ Nouvelle version stable disponible : {} &gt; {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="200"/>
+        <source>FreeCAD is up to date : {}</source>
+        <translation>FreeCAD est à jour : {}</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="204"/>
+        <source>❌ Error recovering the stable version.</source>
+        <translation>❌ Erreur de récupération de la version stable.</translation>
+    </message>
+    <message>
+        <location filename="CheckUpdate.py" line="207"/>
+        <source>Version type not supported : {}</source>
+        <translation>Type de version non supportée : {}</translation>
+    </message>
+</context>
+</TS>
